f172d8aa679d325b7015e3d39f129230b9848cf9
Hampus Liljekvist <3092510+hlilje@users.noreply.github.com>
2024-06-23 21:44:58 +0200

    Add some initial movement and walls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 5f;
+    private float moveSpeed = 5f;
+
+    [SerializeField]
+    private float rotateSpeed = 0.1f;
 
     private PlayerInput playerInput;
     private Rigidbody2D body;
@@ -19,8 +22,16 @@ public class Player : MonoBehaviour
     void Update()
     {
         var move = playerInput.Player.Move.ReadValue<Vector2>();
-        var velocity = speed * move;
+        var look = playerInput.Player.Look.ReadValue<Vector2>();
+
+        var velocity = moveSpeed * move;
+        var rotation = -(look.x * rotateSpeed);
+
         body.velocity = velocity;
+        body.transform.Rotate(0.0f, 0.0f, rotation);
+
+        Debug.Log("Velocity " + velocity);
+        Debug.Log("Rotation " + rotation);
     }
 
     private void OnEnable()
